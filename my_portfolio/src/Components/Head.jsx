5dfbc52e6d56518009390bf6fe5916907624ab7e
sudeepa99 <sudeepa1234nisal@gmail.com>
2024-06-26 23:53:52 +0530

    Imported profile image to the hero section

--- my_portfolio/src/Components/Head.jsx
+++ my_portfolio/src/Components/Head.jsx
@@ -1,18 +1,33 @@
 import React from 'react';
 import "./Head.css";
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import ProfileImage from "../Images/2023_09_23_12_58_IMG_5343.JPG";
 
 export default function Head() {
   return (
     <div>
+      <div className='hero_sec'>
       <div className='head'>
         <div className='welcome_banner'>
-          <h1>Hello, Welcome</h1>
+          <p>Hello, Welcome</p>
         </div>
         <div className='intro'>
-          <h2>I'm Sudeepa Nisal</h2>
+          <p>I'm Sudeepa Nisal</p>
         </div>
-        
       </div>
+      <div className='prof_img'>
+      <Container>
+      <Col xs={6} md={4}>
+          <Image src={ProfileImage} className='img' rounded/>
+        </Col>
+      </Container>
+          
+      </div>
+
+      </div>
+      
       
     </div>
   )
