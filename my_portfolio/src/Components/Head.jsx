7ed3285307452c47aa4b5c708e4bdb1c788f9090
sudeepa99 <sudeepa1234nisal@gmail.com>
2024-06-26 23:21:38 +0530

    Created head section

--- my_portfolio/src/Components/Head.jsx
+++ my_portfolio/src/Components/Head.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import "./Head.css";
 
 export default function Head() {
   return (
     <div>
+      <div className='head'>
+        <div className='welcome_banner'>
+          <h1>Hello, Welcome</h1>
+        </div>
+        <div className='intro'>
+          <h2>I'm Sudeepa Nisal</h2>
+        </div>
+        
+      </div>
       
     </div>
   )
--- my_portfolio/src/Pages/Homepage.jsx
+++ my_portfolio/src/Pages/Homepage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import "./Homepage.css";
 import Navigationbar from '../Components/Navigationbar';
+import Head from '../Components/Head';
 
 export default function Homepage() {
   return (
     <div className='body'>
         <Navigationbar/>
+        <Head/>
       
     </div>
   )
