e041b85b99afe0bc29fea0bc6247001294086741
sudeepa99 <sudeepa1234nisal@gmail.com>
2024-06-26 20:47:06 +0530

    Completed stylings of  navigation bar

--- my_portfolio/src/Components/Navigationbar.jsx
+++ my_portfolio/src/Components/Navigationbar.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import "./Navigationbar.css";
+import BrandName from "../Images/huge (1).png"
+import { Image } from 'react-bootstrap';
 
 export default function Navigationbar() {
   return (
@@ -9,7 +12,7 @@ export default function Navigationbar() {
         <div>
         <Navbar className='nav_Bar'>
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Image src={BrandName} alt='Brand Name'/>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">About</Nav.Link>
