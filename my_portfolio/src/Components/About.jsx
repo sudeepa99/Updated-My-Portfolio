c7d3cd8d04802d4da65327023f1d68205cf94136
sudeepa99 <sudeepa1234nisal@gmail.com>
2024-06-27 00:25:21 +0530

    Done some work in about section

--- my_portfolio/src/Components/About.jsx
+++ my_portfolio/src/Components/About.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React from 'react';
+import "./About.css";
 
 export default function About() {
   return (
     <div>
+      <div className="about-sec">
+        <h1 id='first'>About <span id='second'>Us</span></h1>
+        <p id='third'>
+        I am currently an IT undergraduate at Sabaragamuwa University of Sri Lanka. My passion is to become a Software Engineer because I possess the necessary skills. I am a self-motivated and dedicated undergraduate with a positive attitude toward solving challenging problems and programming. That is why I actively seek out and enroll in new courses and work with tutors in the field of IT, which I complete with great enthusiasm to further my expertise in IT.
+        </p>
+
+      </div>
       
     </div>
   )
