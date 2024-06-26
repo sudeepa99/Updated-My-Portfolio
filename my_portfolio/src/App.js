868589ee278de31d0b3e7042d040b6ff9babf4b1
sudeepa99 <sudeepa1234nisal@gmail.com>
2024-06-26 19:26:40 +0530

    Installed react router dom and created router for homepage

--- my_portfolio/src/App.js
+++ my_portfolio/src/App.js
@@ -1,9 +1,17 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Homepage from './Pages/Homepage';
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Homepage/>}></Route>
+        </Routes>
+      </BrowserRouter>
+
+    </div>
   );
 }
 
