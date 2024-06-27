10d879df4223799379e4561949e8507d6f15d27c
sudeepa99 <sudeepa1234nisal@gmail.com>
2024-06-27 19:42:36 +0530

    Worked with about section of the image

--- my_portfolio/src/Components/About.jsx
+++ my_portfolio/src/Components/About.jsx
@@ -1,16 +1,35 @@
 import React from 'react';
 import "./About.css";
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import CoverIamge from "../Images/Cover_image.png";
 
 export default function About() {
   return (
     <div>
+      <div className='About_section'>
+
+      <div className='cover_img'>
+      <Container>
+      <Col xs={6} md={4}>
+          <Image src={CoverIamge} className='cover_img' rounded/>
+        </Col>
+      </Container>
+
+      </div>
+
       <div className="about-sec">
         <h1 id='first'>About <span id='second'>Us</span></h1>
         <p id='third'>
-        I am currently an IT undergraduate at Sabaragamuwa University of Sri Lanka. My passion is to become a Software Engineer because I possess the necessary skills. I am a self-motivated and dedicated undergraduate with a positive attitude toward solving challenging problems and programming. That is why I actively seek out and enroll in new courses and work with tutors in the field of IT, which I complete with great enthusiasm to further my expertise in IT.
+        I'm an undergraduate at Sabaragamuwa University of Sri Lanka with a passion for software engineering. Enthusiastic about coding and problem-solving, I'm eager to innovate and contribute to the tech world.
         </p>
 
       </div>
+
+      </div>
+      
+      
       
     </div>
   )
