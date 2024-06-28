e6147b581c58df57679608e1d49d0c0ba59cc85f
sudeepa99 <sudeepa1234nisal@gmail.com>
2024-06-29 00:43:22 +0530

    Portfolio section almot finalized

--- my_portfolio/src/Components/Portfolio.jsx
+++ my_portfolio/src/Components/Portfolio.jsx
@@ -4,7 +4,10 @@ import CardDesign from './CardDesign';
 import StockMngImg from "../Images/stock_mng.png";
 import FilmBookImg from "../Images/film_booking.png";
 import MyPortImg from "../Images/Portfolio.png";
-import Carex from "../Images/carex.png";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons';
+import { Button } from 'react-bootstrap';
+
 
 export default function Portfolio() {
   return (
@@ -16,8 +19,27 @@ export default function Portfolio() {
         <CardDesign Image_name={StockMngImg} port_title="Stock Management System"/>
         <CardDesign Image_name={FilmBookImg} port_title="Film Booking Website"/>
         <CardDesign Image_name={MyPortImg} port_title="My portfolio"/>
-        
-        
+      </div>
+      <div style={{
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      marginTop:"2%"
+      
+      }}>
+        <Button 
+        style={{
+        backgroundColor:"#6B7280",
+        borderColor:"black", 
+        borderWidth:"2px",
+        }}
+        variant='primary'>See More
+        <FontAwesomeIcon icon={faAngleDoubleRight} 
+        style={{
+        marginLeft:"4px",
+        }}/>
+        </Button>
+      
       </div>
       
     </div>
