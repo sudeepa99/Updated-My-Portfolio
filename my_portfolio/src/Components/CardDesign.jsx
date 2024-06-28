296f8dfad3e2cbb026143fa978fe19855579ca27
sudeepa99 <sudeepa1234nisal@gmail.com>
2024-06-29 00:19:30 +0530

    Added stylings to portfolio section

--- my_portfolio/src/Components/CardDesign.jsx
+++ my_portfolio/src/Components/CardDesign.jsx
@@ -6,12 +6,20 @@ import Card from 'react-bootstrap/Card';
 
 export default function CardDesign(props) {
   return (
-    <div>
-        <Card style={{ width: '24rem', height:'24rem', backgroundColor:'#111827' }}>
+    <div style={{marginTop: "4%"}}>
+        <Card style={{ width: '24rem', height:'20rem', backgroundColor:'#111827' }}>
         <Card.Img style={{width:'24rem'  }} variant="top" src={props.Image_name}  />
-        <Card.Body>
-        <Card.Title style={{color:"#ffff"}}>{props.port_title}</Card.Title>
-        <Button variant="primary">More Details</Button>
+        <Card.Body style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <Card.Title style={{color:"#ffff", alignItems:"start"}}>{props.port_title} </Card.Title>
+        <Button style={{
+          backgroundColor: "#6B7280",
+          borderColor:"black", 
+          borderWidth:"2px",  
+          textAlign: "center",
+          marginTop: "2%" 
+          }}
+          variant="primary">More Details
+        </Button>
         </Card.Body>
         </Card>
 
