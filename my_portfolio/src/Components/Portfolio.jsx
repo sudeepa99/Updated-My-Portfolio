fee05855f75e5254227fb253159eac2774287dc1
sudeepa99 <sudeepa1234nisal@gmail.com>
2024-06-29 14:33:52 +0530

    added more features to the project

--- my_portfolio/src/Components/Portfolio.jsx
+++ my_portfolio/src/Components/Portfolio.jsx
@@ -17,17 +17,17 @@ export default function Portfolio() {
         <h1><span id='other'>My</span> Portfolio</h1>
       </div>
       <div className='card_container'>
-        <CardDesign  Image_name={StockMngImg} port_title="Stock Management System" />
-        <CardDesign  Image_name={FilmBookImg} port_title="Film Booking Website"/>
-        <CardDesign  Image_name={MyPortImg} port_title="My portfolio"/>
-        <CardDesign  Image_name={SoluxeImg} port_title="Soluxe E-commerce System"/>
+        <CardDesign  Image_name={StockMngImg} port_title="Stock Management System" repo_link="https://github.com/sudeepa99/Stock-Management-System" />
+        <CardDesign  Image_name={FilmBookImg} port_title="Film Booking Website" repo_link="https://github.com/isurubandara1/MoviesTicketBooking_Website"/>
+        <CardDesign  Image_name={MyPortImg} port_title="My portfolio" repo_link="https://github.com/sudeepa99/Updated-My-Portfolio"/>
+        <CardDesign  Image_name={SoluxeImg} port_title="Soluxe E-commerce System" repo_link="https://github.com/sudeepa99/Soluxe"/>
 
       <div style={{
       display: 'flex', 
       justifyContent: 'center', 
       alignItems: 'center', 
       marginTop:"2%",
-      marginLeft:"9%"
+      marginLeft:"12%"
       
       }}>
         <Button 
@@ -37,7 +37,9 @@ export default function Portfolio() {
         borderWidth:"2px",
         fontSize:"24px",
         }}
-        variant='primary'>See More
+        variant='primary'
+        href="https://github.com/sudeepa99"
+        >See More
         <FontAwesomeIcon icon={faAngleDoubleRight} 
         style={{
         marginLeft:"4px",
