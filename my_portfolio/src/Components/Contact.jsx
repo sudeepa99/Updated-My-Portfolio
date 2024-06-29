ddb26b23040c901e2a4d9d31121dea70122e416f
sudeepa99 <sudeepa1234nisal@gmail.com>
2024-06-29 10:56:13 +0530

    Imported a form to contact section

--- my_portfolio/src/Components/Contact.jsx
+++ my_portfolio/src/Components/Contact.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import "./Contact.css";
+import FormDesign from './FormDesign';
 
 export default function Contact() {
   return (
     <div className='contact_sec'>
       <div className='contact_container'>
       <h1 className='contact_head'><span id='other'>Contact</span> Me</h1>
+      <FormDesign/>
 
       </div>
 
