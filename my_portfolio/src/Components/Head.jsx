10bef69a6d71b493f27f76b228c1756c74363de2
sudeepa99 <sudeepa1234nisal@gmail.com>
2024-07-01 11:24:06 +0530

    Added an animation to cover image

--- my_portfolio/src/Components/Head.jsx
+++ my_portfolio/src/Components/Head.jsx
@@ -4,6 +4,7 @@ import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
 import ProfileImage from "../Images/profile-img.png";
+import { motion } from 'framer-motion';
 
 export default function Head() {
   return (
@@ -20,7 +21,15 @@ export default function Head() {
       <div className='prof_img'>
       <Container>
       <Col xs={6} md={4}>
-          <Image src={ProfileImage} className='img' rounded/>
+          <motion.img 
+          src={ProfileImage}
+          alt="Profile Image"
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+          whileHover={{ scale: 1.05 }}
+          className='img' rounded
+          />
         </Col>
       </Container>
           
