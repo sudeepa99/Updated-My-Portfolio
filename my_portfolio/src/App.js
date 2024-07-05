46d45029d8217f070fe38f152cde691e683d7278
sudeepa99 <sudeepa1234nisal@gmail.com>
2024-07-05 22:30:16 +0530

    Done some updates in navigation bar and about section

--- my_portfolio/src/App.js
+++ my_portfolio/src/App.js
@@ -1,21 +1,19 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Homepage from './Pages/Homepage';
-import { ParallaxProvider } from 'react-scroll-parallax';
-import About from './Components/About';
 
 function App() {
   return (
-    <ParallaxProvider>
-      <div>
-        <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<Homepage />} />
-            <Route path='/about' element={<About />} />
-          </Routes>
-        </BrowserRouter>
-      </div>
-    </ParallaxProvider>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Homepage/>}></Route>
+        </Routes>
+      </BrowserRouter>
+
+      
+
+    </div>
   );
 }
 
